862280bd84e346c926d4e115b587cb73256116ec
Maximilian Neßlauer <dev@max-nesslauer.com>
2026-06-16 18:41:42 +0200

    Bump tested game version to v2026.6.7.4631

--- StageInfo/Mod.cs
+++ StageInfo/Mod.cs
@@ -14,7 +14,7 @@ public sealed class Mod
 {
     private static Harmony? _harmony;
 
-    private const string TestedGameVersion = "v2026.6.6.4601";
+    private const string TestedGameVersion = "v2026.6.7.4631";
 
     [StarMapAllModsLoaded]
     public void OnFullyLoaded()
